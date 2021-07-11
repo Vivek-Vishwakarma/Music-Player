a340c3998896112f092a863e92f7c484a9cda9e4
Vivek Vishwakarma <56250859+Vivek-Vishwakarma@users.noreply.github.com>
2021-07-11 17:58:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const next = document.getElementById('forward')
 const container = document.getElementById('container')
 const audio = document.querySelector('#audio')
 const title = document.querySelector('#title')
-const cover =  document.getElementById('cover')
+const cover = document.getElementById('cover')
+const progress = document.querySelector('.progress-id')
+const progressContainer = document.querySelector('.progress')
+
 
 const songs = ["faded", "payphone", "mushoku"]
 
@@ -12,49 +15,65 @@ let songInd = 0;
 
 load(songs[songInd])
 
-function load(song){
+function load(song) {
     title.innerHTML = song;
     audio.src = `music/${song}.mp3`
     cover.src = `images/${song}.jpg`
 }
 
-function playSong(){
+function playSong() {
     container.classList.add('play')
     play.innerHTML = '<i class="fas fa-pause"></i>'
     audio.play()
 }
-function pauseSong(){
+function pauseSong() {
     container.classList.remove('play')
     play.innerHTML = '<i class="fas fa-play"></i>'
     audio.pause()
 }
-function nextSong(){
+function nextSong() {
     songInd++
-    if(songInd > (songs.length-1)){
+    if (songInd > (songs.length - 1)) {
         songInd = 0
     }
     load(songs[songInd])
     playSong()
 }
-function prevSong(){
+function prevSong() {
     songInd--
-    if(songInd < 0){
-        songInd = (songs.length-1)
+    if (songInd < 0) {
+        songInd = (songs.length - 1)
     }
     load(songs[songInd])
     playSong()
+}
+function progressPer(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+function setProgress(e){
+    const width = this.clientWidth
+    const click = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (click/width)*duration
 }
 
 play.addEventListener("click", () => {
     const playing = container.classList.contains('play')
 
-    if(playing){
+    if (playing) {
         pauseSong()
     }
-    else{
+    else {
         playSong()
     }
 })
 
 next.addEventListener("click", nextSong)
 back.addEventListener("click", prevSong)
+audio.addEventListener("timeupdate", progressPer)
+progressContainer.addEventListener('click', setProgress)
+audio.addEventListener('ended', nextSong)
+
